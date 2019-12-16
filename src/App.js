41757656d913fade5e678d16cd795ca2e9f5896f
Argetim Ramadani <argdev@outlook.com>
2019-12-16 23:09:00 +0100

    Added HTTP requests to a REST Api using Axios

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 
-import uuid from 'uuid';
+//import uuid from 'uuid';
 
 import './App.css';
 
 class App extends React.Component {
 	state = {
-		todos: [
-			{
-				id: uuid.v4(),
-				title: 'Learn Vanilla JS',
-				completed: false
-			},
-			{
-				id: uuid.v4(),
-				title: 'Learn ReactJS',
-				completed: false
-			},
-			{
-				id: uuid.v4(),
-				title: 'Learn ExpressJS',
-				completed: false
-			}
-		]
+		todos: []
 	};
+
+	componentDidMount() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+			.then(res => this.setState({ todos: res.data }));
+	}
 
 	// Toggle Complete
 	toggleComplete = id => {
@@ -43,18 +34,20 @@ class App extends React.Component {
 	};
 
 	deleteItem = id => {
-		this.setState({
-			todos: [...this.state.todos.filter(todo => todo.id !== id)]
-		});
+		axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res =>
+			this.setState({
+				todos: [...this.state.todos.filter(todo => todo.id !== id)]
+			})
+		);
 	};
 
 	addTodo = title => {
-		const newTodo = {
-			id: uuid.v4(),
-			title,
-			completed: false
-		};
-		this.setState({ todos: [...this.state.todos, newTodo] });
+		axios
+			.post('https://jsonplaceholder.typicode.com/todos', {
+				title,
+				completed: false
+			})
+			.then(res => this.setState({ todos: [...this.state.todos, res.data] }));
 	};
 
 	render() {
