fd902345510267e081c8795cbeb6e08dcbe7e6d1
Argetim Ramadani <argdev@outlook.com>
2019-12-16 20:38:15 +0100

    Created Todos, TodoItem & Header components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Learn Vanilla JS',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'Learn ReactJS',
+				completed: false
+			},
+			{
+				id: 3,
+				title: 'Learn ExpressJS',
+				completed: false
+			}
+		]
+	};
+
+	// Toggle Complete
+	toggleComplete = id => {
+		this.setState({
+			todos: this.state.todos.map(todo => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		});
+	};
+
+	deleteItem = id => {
+		this.setState({
+			todos: [...this.state.todos.filter(todo => todo.id !== id)]
+		});
+	};
+	//57:00min
+	render() {
+		return (
+			<div className='App'>
+				<Header />
+				<Todos
+					todos={this.state.todos}
+					toggleComplete={this.toggleComplete}
+					deleteItem={this.deleteItem}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
